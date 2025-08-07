c4cfd7fbe60afe4d4d01f29444a2bd096ee2c029
daiki.tabata <daiki.tabata@datafluct.com>
2025-08-08 00:15:23 +0900

    ロギング修正

--- src/lambda/wms_refresh_token/handler.py
+++ src/lambda/wms_refresh_token/handler.py
@@ -1,21 +1,28 @@
 import json
+import logging
 import os
 import sys
 from logging import getLogger
 
-import requests
-
+logging.basicConfig(level=logging.INFO)
 logger = getLogger(__name__)
 
 
 def entrypoint(event, context):
+    logger.info("entrypoint called")
     logger.info(f"ENV={os.getenv('ENV', '')}")
-    response = requests.get("https://example.com/", timeout=30)
-    result = 1 / 0  # 0除算でエラーを発生させる
+
+    try:
+        result = 1 / 0  # 0除算でエラーを発生させる
+
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+        raise
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "status_code": response.status_code,
+            "status_code": 200,
             "message": f'Hello World From AWS Lambda using Python {sys.version}.'
         })
     }
